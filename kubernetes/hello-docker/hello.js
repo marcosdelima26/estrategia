e415fcfd859f816bdf7982a0eac8257bd4fc7573
Raphael Lacerda - lacerdaph <lacerdaph@gmail.com>
2021-06-08 23:14:59 -0300

    chart

--- kubernetes/hello-docker/hello.js
+++ kubernetes/hello-docker/hello.js
@@ -4,6 +4,6 @@ const port = 3000
 
 let contador = 0;
 
-app.get('/', (req, res) => res.send('Hello World! versao 1.0.0 - requisicao número: '+contador++))
+app.get('/', (req, res) => res.send('Hello World! versao 2.0.0 - requisicao número: '+contador++))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
